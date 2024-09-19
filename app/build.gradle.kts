ffd252e6a83e9301ca0963716b72bdee8896a856
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2024-09-19 07:28:07 +0530

    ok
    comit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.mbexample.alarmmanager"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mbexample.alarmmanager"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
